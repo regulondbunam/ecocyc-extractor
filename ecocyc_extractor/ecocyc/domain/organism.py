281c8c63660284936b1d1f206bf427d424c20dc9
Phill Betancourt <Felipe.Bet@Hotmail.com>
2025-11-25 14:34:59 -0600

    FIX: Remove unused properties from collection schema
    Several properties were not referenced in the JSON Schema and therefore were not
    required by the collection domain model.
    
    ### Added
    - Without changes.
    
    ### Changed
    - Without changes.
    
    ### Deprecated
    - Without changes.
    
    ### Fixed
    - Removed unused fields: `citations` and `db_links` from the collection payload.
    
    ### To Fix
    - The `genome` property remains as an ID only, since there is no canonical name
      associated with the original EcoCyc frame object.

--- ecocyc_extractor/ecocyc/domain/organism.py
+++ ecocyc_extractor/ecocyc/domain/organism.py
@@ -10,14 +10,14 @@ from .base import Base
 from ..utils import utils
 
 
-class Organism(object):
+class Organism(Base):
     # Connection to the database or management system, similar to the one used in Operon
     pt_connection = Base.pt_connection
 
     def __init__(self, **kwargs):
         self.id = kwargs.get("id", None)
-        self.citations = kwargs.get("citations", None)
-        self.db_links = kwargs.get("db_links", None)
+        # self.citations = kwargs.get("citations", None)
+        # self.db_links = kwargs.get("db_links", None)
         self.comment = kwargs.get("comment", None)
         self.genome = kwargs.get("genome", None)
         self.comment_internal = kwargs.get("comment_internal", None)
@@ -31,8 +31,46 @@ class Organism(object):
 
     # --- Properties with Getters and Setters ---
     # TODO: Add DB_LINKS
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if name is None:
+            name = self.id.replace("|", "")
+        self._name = name
+
+    @property
+    def genome(self):
+        return self._genome
+
+    @genome.setter
+    def genome(self, genome):
+        genome = genome
+        if genome is not None:
+            genome = genome[0]
+            genome_name = Organism.pt_connection.get_slot_value(genome, "|COMMON-NAME|")
+            genome_name = None
+            if genome_name is None:
+                self._genome = genome_name
+        self._genome = genome
+
+    @property
+    def synonyms(self):
+        return self._synonyms
+
+    @synonyms.setter
+    def synonyms(self, synonyms=None):
+        gene_id = self.id.replace("|", "")
+        self._synonyms = synonyms
+        try:
+            self._synonyms.append(gene_id)
+        except AttributeError:
+            self._synonyms = [gene_id]
 
     @staticmethod
     def get_only_properties_with_values(properties):
         properties = {k: v for k, v in properties.items() if v is not None}
         return properties
+
--- ecocyc_extractor/regulondb/organisms.py
+++ ecocyc_extractor/regulondb/organisms.py
@@ -14,21 +14,21 @@ def get_regulondb_organisms(only_properties_with_values=False, organism_name='EC
     organisms = Organisms(organism_name)
 
     for organism in organisms.objects:
-        organism_object = {
-            "_id": organism.id,
-            "citations": organism.citations,
-            "description": organism.comment,
-            # "externalCrossReferences": organism.db_links,
-            "genome": organism.genome,
-            "internalComment": organism.comment_internal,
-            "name": organism.name,
-            "pgdbAuthors": organism.pgdb_authors,
-            "pgdbCopyright": organism.pgdb_copyright,
-            "pgdbName": organism.pgdb_name,
-            "strainName": organism.strain_name,
-            "synonyms": organism.synonyms,
-            "url": organism.url
-        }
         if only_properties_with_values is True:
+            organism_object = {
+                "_id": organism.id,
+                # "citations": organism.citations,
+                # "externalCrossReferences": organism.db_links,
+                "description": organism.comment,
+                "genome": organism.genome,
+                "internalComment": organism.comment_internal,
+                "name": organism.name,
+                "pgdbAuthors": organism.pgdb_authors,
+                "pgdbCopyright": organism.pgdb_copyright,
+                "pgdbName": organism.pgdb_name,
+                "strainName": organism.strain_name,
+                "synonyms": organism.synonyms,
+                "url": organism.url
+            }
             organism_object = organism.get_only_properties_with_values(organism_object)
         yield organism_object
